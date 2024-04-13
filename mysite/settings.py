514e5646407bf56f36a9c47eebb4da61e14c5bf3
Your Name <you@example.com>
2024-04-13 16:19:08 -0400

    M05 Tutorial - Django Deploy and Views

--- mysite/settings.py
+++ mysite/settings.py
@@ -48,7 +48,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
 ]
+
 
 ROOT_URLCONF = 'mysite.urls'
 
